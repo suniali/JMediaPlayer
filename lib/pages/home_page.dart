30c20a28a0d082ede82a4abe1185dba7f5cee7ec
AliMatroky <alimatroky@gmail.com>
2019-02-01 21:25:24 +0330

    create tabs and pagers

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
-
 import 'package:flutter/material.dart';
+import 'package:jmedia_player/component/album_page.dart';
+import 'package:jmedia_player/component/offline_page.dart';
 import 'package:jmedia_player/component/play_now.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -10,26 +11,60 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  List<Tab> tabs = [
+    Tab(text: "Offline", icon: Icon(Icons.offline_bolt)),
+    Tab(text: "Album", icon: Icon(Icons.album)),
+    Tab(text: "All", icon: Icon(Icons.all_inclusive)),
+    Tab(text: "favarits", icon: Icon(Icons.favorite)),
+  ];
+  List<Widget> pagerWidget = [];
+  TabController tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    tabController = TabController(vsync: this, length: tabs.length);
+    pagerWidget.add(OfflinePage());
+    pagerWidget.add(AlbumPage());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 0.0,
-          backgroundColor: Colors.transparent,
-          iconTheme: IconThemeData(color: Colors.black),
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () {},
-          ),
-          actions: <Widget>[],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        bottom: TabBar(
+          controller: tabController,
+          tabs: tabs,
         ),
-        body: Container(
-          color: Colors.black54,
-          child: PlayNow(),
-        )
+      ),
+      drawer: Drawer(),
+      //     body: NestedScrollView(
+      //   headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+      //     return <Widget>[
+      //       SliverAppBar(
+      //         expandedHeight: 100.0,
+      //         backgroundColor: Colors.black87,
+      //         pinned: false,
+      //         flexibleSpace: FlexibleSpaceBar(
+      //           //background
+      //         ),
+      //       )
+      //     ];
+      //   },
+      //   body: Container(
+      //     color: Colors.black54,
+      //     child: PlayNow(),
+      //   ),
+      // )
+      body: TabBarView(
+        controller: tabController,
+        children: pagerWidget,
+      ),
     );
   }
 }
-
-
